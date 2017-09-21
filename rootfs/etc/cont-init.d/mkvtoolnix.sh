47fe5e5e5173b6dd127b70117e7d1fd9ad6d4142
Jocelyn Le Sage <jocelyn.lesage@hpe.com>
2017-09-21 06:55:30 -0400

    Added support for interface languages.

--- rootfs/etc/cont-init.d/mkvtoolnix.sh
+++ rootfs/etc/cont-init.d/mkvtoolnix.sh
@@ -13,6 +13,7 @@ mkdir -p "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui"
 [ ! -f /config/mkvtoolnix-gui.ini ] || mv -v /config/mkvtoolnix-gui.ini "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/"
 [ ! -d /config/jobQueue ] || mv -v /config/jobQueue "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/"
 [ ! -f /config/QtProject.conf ] || mv -v /config/QtProject.conf "$XDG_CONFIG_HOME/"
+[ ! -f "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/mkvtoolnix-gui.ini" ] || sed -i 's/^uiLocale=$/uiLocale=en_US/' "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/mkvtoolnix-gui.ini"
 
 # Copy default configuration files if needed.
 [ -f "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/mkvtoolnix-gui.ini" ] || cp -v /defaults/mkvtoolnix-gui.ini "$XDG_CONFIG_HOME/bunkus.org/mkvtoolnix-gui/"

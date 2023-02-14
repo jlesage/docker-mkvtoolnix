d3483c1b95c2e2ad45f5b038493fa5bf8b790399
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-02-13 21:19:34 -0500

    Workaround for the following crash: ```    terminate called after throwing an instance of 'std::runtime_error'      what():  locale::facet::_S_create_c_locale name not valid ```

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Needed to avoid the following error:
+#   terminate called after throwing an instance of 'std::runtime_error'
+#     what():  locale::facet::_S_create_c_locale name not valid
+export LC_ALL=C
+
 if [ -f /config/xdg/config/bunkus.org/mkvtoolnix-gui/mkvtoolnix-gui.ini ]
 then
     UI_LOCAL=$(cat /config/xdg/config/bunkus.org/mkvtoolnix-gui/mkvtoolnix-gui.ini | grep "^uiLocal" | cut -d'=' -f2)

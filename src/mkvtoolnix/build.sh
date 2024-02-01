7d74a57415ea987a3dfafb6de5b85fb9c97dfbb1
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-02-01 17:14:46 -0500

    Fixed cross-compilation.

--- src/mkvtoolnix/build.sh
+++ src/mkvtoolnix/build.sh
@@ -14,6 +14,9 @@ export CXX=xx-clang++
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Set to `1` to enable verbose build.
+VERBOSE_BUILD=0
+
 function log {
     echo ">>> $*"
 }
@@ -39,6 +42,8 @@ apk --no-cache add \
     pkgconf \
     qtchooser \
     qt6-qtbase-dev \
+    qt6-qtmultimedia-dev \
+    qt6-qtsvg-dev \
 
 xx-apk --no-cache --no-scripts add \
     musl-dev \
@@ -73,19 +78,37 @@ curl -# -L -f ${MKVTOOLNIX_URL} | tar xJ --strip 1 -C /tmp/mkvtoolnix
 log "Patching MKVToolNix..."
 patch -p1 -d /tmp/mkvtoolnix < "$SCRIPT_DIR"/locale-fix.patch
 
+# NOTE: During the configure phase, Qt6 related checks (`ac/qt6.m4`) are done
+#       with qmake6, which doesn't work for cross-compilation.  Thus, the checks
+#       are done against the builder host's Qt6 installation (not against the
+#       target one).
 log "Configuring MKVToolNix..."
+sed -i 's/$${CROSS_COMPILE}clang/xx-clang/g' /usr/lib/qt6/mkspecs/common/clang.conf
 (
     cd /tmp/mkvtoolnix && \
     env LIBINTL_LIBS=-lintl ./configure \
+        am_cv_qt6_compilation=1 \
         --build=$(TARGETPLATFORM= xx-clang --print-target-triple) \
         --host=$(xx-clang --print-target-triple) \
         --prefix=/usr \
         --disable-update-check \
         --with-boost-libdir=$(xx-info sysroot)/usr/lib \
+        --with-qmake6=/usr/bin/qmake6 \
 )
 
+# Paths used in QT_FLAGS, QT_LIBS and QT_LIBS_NON_GUI variables (defined in
+# build-config) need to be adjusted to use the ones of the build target.
+sed -i "s|-I/usr/|-I$(xx-info sysroot)usr/|g" /tmp/mkvtoolnix/build-config
+sed -i "s| /usr/lib/| $(xx-info sysroot)usr/lib/|g" /tmp/mkvtoolnix/build-config
+sed -i "s| -Wl,-rpath,/usr/lib | -Wl,-rpath-link,$(xx-info sysroot)usr/lib:$(xx-info sysroot)usr/lib/pulseaudio |g" /tmp/mkvtoolnix/build-config
+
+# Make sure to use tools from the builder host.
+sed -i "s|MOC = .*|MOC = /usr/lib/qt6/libexec/moc|" /tmp/mkvtoolnix/build-config
+sed -i "s|RCC = .*|RCC = /usr/lib/qt6/libexec/rcc|" /tmp/mkvtoolnix/build-config
+sed -i "s|UIC = .*|UIC = /usr/lib/qt6/libexec/uic|" /tmp/mkvtoolnix/build-config
+
 log "Compiling MKVToolNix..."
-rake -f /tmp/mkvtoolnix/Rakefile -j$(nproc)
+rake V=${VERBOSE_BUILD:-0} -f /tmp/mkvtoolnix/Rakefile -j$(nproc)
 
 log "Installing MKVToolNix..."
 DESTDIR=/tmp/mkvtoolnix-install rake -f /tmp/mkvtoolnix/Rakefile install
